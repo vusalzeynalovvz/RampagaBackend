f90feb735d64411d5cf5be99b9ea5e087eb24e5f
vusalzeynalovvz <vusalvz@code.edu.az>
2024-03-17 14:18:14 +0400

    added bacpac file

--- Rampage/obj/Debug/net6.0/Rampage.AssemblyInfo.cs
+++ Rampage/obj/Debug/net6.0/Rampage.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rampage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05f108fb6bbcfd6fd0c61d3ad489fea2a06b7f49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d499f13c19fa968cb0fe84920cba9a7868fed449")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rampage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rampage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
